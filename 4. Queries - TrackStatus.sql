393b789880d86fde1c859306e33059942c90c082
Andrew <andrew.laery@gmail.com>
2017-08-08 13:33:13 +0200

    changes

--- 4. Queries - TrackStatus.sql
+++ 4. Queries - TrackStatus.sql
@@ -1,27 +1,8 @@
 -- Creates a view of current status of a members communication track progress
+
+
 DROP VIEW IF EXISTS V_COMMUNICATIONTRACKSTATUS0;
 CREATE VIEW V_COMMUNICATIONTRACKSTATUS0 AS
-    SELECT
-      NameFull ,
-      Track ,
-      QualificationsOrder ,
-      Qualification ,
-      QualificationStatus ,
-      ManualGroupsOrder ,
-      ManualGroup ,
-      ManualsOrder ,
-      Manual ,
-      ProjectsOrder ,
-      Project ,
-      RolesOrder ,
-      Role ,
-      Date1
-	FROM V_MOSTRECENTPROJECT0
-	ORDER BY NameFull , QualificationsOrder , ManualGroupsOrder , ManualsOrder , ProjectsOrder;
-SELECT * FROM V_COMMUNICATIONTRACKSTATUS0;
-
-DROP VIEW IF EXISTS V_COMMUNICATIONTRACKSTATUS1;
-CREATE VIEW V_COMMUNICATIONTRACKSTATUS1 AS
   SELECT
       NameFull ,
       Track ,
@@ -32,7 +13,7 @@ CREATE VIEW V_COMMUNICATIONTRACKSTATUS1 AS
       Role ,
       Date1 ,
       QualificationStatus
-  FROM V_COMMUNICATIONTRACKSTATUS0
+  FROM V_MOSTRECENTPROJECT0
   WHERE
     NameFull = 'Peter Webster'
     AND Track = 'Communication'
@@ -40,5 +21,26 @@ CREATE VIEW V_COMMUNICATIONTRACKSTATUS1 AS
     AND (Qualification <> 'No qualification')
     AND (Role <> 'No role')
     ORDER BY NameFull , QualificationsOrder , ManualGroupsOrder , ManualsOrder , ProjectsOrder;
+SELECT * FROM V_COMMUNICATIONTRACKSTATUS0;
 
-SELECT * FROM V_COMMUNICATIONTRACKSTATUS1;
+DROP VIEW IF EXISTS V_LEADERSHIPTRACKSTATUS0;
+CREATE VIEW V_LEADERSHIPTRACKSTATUS0 AS
+  SELECT
+      NameFull ,
+      Track ,
+      Qualification ,
+      ManualGroup
+      Manual ,
+      Project ,
+      Role ,
+      Date1 ,
+      QualificationStatus
+  FROM V_MOSTRECENTPROJECT0
+  WHERE
+    NameFull = 'Peter Webster'
+    AND Track = 'Leadership'
+    AND QualificationStatus = 'In progress'
+    AND (Qualification <> 'No qualification')
+    AND (Role <> 'No role')
+    ORDER BY NameFull , QualificationsOrder , ManualGroupsOrder , ManualsOrder , ProjectsOrder;
+SELECT * FROM V_LEADERSHIPTRACKSTATUS0;
