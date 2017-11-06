597faf0265c6170c464e9a8f30be089d20a6c8f8
Andrew <andrew.laery@gmail.com>
2017-11-07 07:34:40 +1300

    updates

--- Regular Update Script.sql
+++ Regular Update Script.sql
@@ -1,10 +1,11 @@
 
 
 -- Change all 'forecast' projects to 'forecast_old' so the filters work.
+SET @today = current_date();
 SET SQL_SAFE_UPDATES = 0;
 UPDATE RECORDS_PROJECTS
 SET itemstatus = 'forecast_old'
-WHERE
-	MeetingDate < '2017-11-06'
+WHERE 
+	MeetingDate < @today
 	AND itemstatus = 'forecast';
 SET SQL_SAFE_UPDATES = 1;
